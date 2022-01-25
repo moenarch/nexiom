786da3bdb0b7167a568dfb163234eb5da47da6e9
moenarch <cathmoelic@gmail.com>
2022-01-25 05:54:34 +0100

    let's go

---
+++ react/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import markdown from './Introduction.md';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { text: null }
+  }
+
+  componentWillMount() {
+    fetch(markdown)
+      .then((response) => response.text())
+      .then((text) => {
+        this.setState({ text: text })
+      });
+  }
+
+  render() {
+    return (<div>
+      <h1>What up?</h1>
+      <ReactMarkdown children={this.state.text} remarkPlugins={[remarkGfm]} />
+    </div>
+    );
+  }
+}
+
+export default App;
